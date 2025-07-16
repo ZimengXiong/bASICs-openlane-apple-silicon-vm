65c313ec2e916f89ae3f86e8242e1de24f63b2a5
Zimeng Xiong <zxzimeng@gmail.com>
2025-07-16 08:47:15 -0700

    add apply all patches script

---
+++ applypatches.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Run with bash <(curl -s https://raw.githubusercontent.com/ZimengXiong/bASICs-openlane-apple-silicon-vm/refs/heads/main/applypatches.sh) 
+
+bash <(curl -s https://raw.githubusercontent.com/ZimengXiong/bASICs-openlane-apple-silicon-vm/refs/heads/main/patch-2/patch-2.sh) 
+bash <(curl -s https://raw.githubusercontent.com/ZimengXiong/bASICs-openlane-apple-silicon-vm/refs/heads/main/patch-1.sh)
